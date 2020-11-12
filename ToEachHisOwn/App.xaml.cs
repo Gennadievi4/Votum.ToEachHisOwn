c14b7a696d35d3c3d63d7ddc579cfbd72bac1f13
Геннадий Тертычный <gonzyck@gmail.com>
2020-11-12 19:28:59 +0400

    Создал инфраструктуру IoC+DI.

--- ToEachHisOwn/App.xaml.cs
+++ ToEachHisOwn/App.xaml.cs
@@ -1,17 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace ToEachHisOwn
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static IHost __Host;
+        public static IHost Host => __Host ?? (__Host = Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build());
+        public static bool IsDesignMode { get; private set; } = true;
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            IsDesignMode = false;
+
+            var host = Host;
+            base.OnStartup(e);
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            __Host = null;
+        }
+
+        public static void ConfigureServies(HostBuilderContext host, IServiceCollection services)
+        {
+
+        }
+
+        public static string CurrentDirectory => IsDesignMode ? Path.GetDirectoryName(GetSourceCodePath()) : Environment.CurrentDirectory;
+
+        private static string GetSourceCodePath([CallerFilePath] string Path = null) => Path;
     }
 }
