b2539375282e68cb36abec06302459c10765bcae
Геннадий Тертычный <gonzyck@gmail.com>
2020-11-13 21:45:43 +0400

    Добавил команду закрытия окна, обощил команду лямбда, добавил фонт осом

---
+++ ToEachHisOwn/Infrastructure/Commands/CloseWindowCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace ToEachHisOwn.Infrastructure.Commands
+{
+    internal class CloseWindowCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object p)
+        {
+            var window = p as Window;
+
+            if (window is null)
+                window = Application.Current.Windows
+                    .Cast<Window>()
+                    .FirstOrDefault(x => x.IsFocused);
+
+            if (window is null)
+                window = Application.Current.Windows
+                    .Cast<Window>()
+                    .FirstOrDefault(x => x.IsActive);
+
+            window?.Close();
+        }
+    }
+}
--- ToEachHisOwn/Infrastructure/Commands/LambdaCommand.cs
+++ ToEachHisOwn/Infrastructure/Commands/LambdaCommand.cs
@@ -2,19 +2,19 @@
 
 namespace ToEachHisOwn.Infrastructure.Commands
 {
-    internal class LambdaCommand : Command
+    internal class LambdaCommand<T> : Command
     {
-        private readonly Action<object> _Execute;
-        private readonly Func<object, bool> _CanExecute;
+        private readonly Action<T> _Execute;
+        private readonly Func<T, bool> _CanExecute;
 
-        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        public LambdaCommand(Action<T> Execute, Func<T, bool> CanExecute = null)
         {
             _Execute = Execute ?? throw new ArgumentException(nameof(Execute));
             _CanExecute = CanExecute;
         }
 
-        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke((T)parameter) ?? true;
 
-        public override void Execute(object parameter) => _Execute(parameter);
+        public override void Execute(object parameter) => _Execute((T)parameter);
     }
 }
