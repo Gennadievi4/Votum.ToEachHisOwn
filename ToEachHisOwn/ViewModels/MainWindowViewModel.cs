4616cbf4fcafc429b1e67ca878fcee06afe7fa8e
Геннадий Тертычный <gonzyck@gmail.com>
2020-12-21 00:57:11 +0400

    Поразбирался с Меню от MD, реализовал команду закрытия приложения.

--- ToEachHisOwn/ViewModels/MainWindowViewModel.cs
+++ ToEachHisOwn/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,18 @@
-﻿using ToEachHisOwn.ViewModels.Base;
+﻿using System.Windows.Input;
+using ToEachHisOwn.Infrastructure.Commands;
+using ToEachHisOwn.ViewModels.Base;
 
 namespace ToEachHisOwn.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Fields
         private string _Title;
         public string Title => _Title = "Каждому своё!";
+        #endregion
+
+        #region Commands
+        public ICommand ClossApp => new CloseWindowCommand();
+        #endregion
     }
 }
