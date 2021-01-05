2d97325df20594da52a12537f04896290d1500bd
Геннадий Тертычный <gonzyck@gmail.com>
2021-01-05 23:41:07 +0300

    Эксперимент с потоками async/await, Thread.

--- TestConcole/Program.cs
+++ TestConcole/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -56,25 +57,6 @@ namespace TestConcole
         }
         #endregion
 
-        #region Эксперименты с потоками №1
-
-        //private static bool __CanClockWork = true;
-
-        //private static void StartConsoleHeaderClock()
-        //{
-        //    var thread = Thread.CurrentThread;
-        //    Console.WriteLine($"Запущен поток: id:{thread.ManagedThreadId}, name:{thread.Name}, priority:{thread.Priority}");
-
-        //    while (__CanClockWork)
-        //    {
-        //        Console.Title = DateTime.Now.ToString("G");
-        //        Thread.Sleep(100);
-        //    }
-
-        //    Console.WriteLine("Поток часов завершил свою работу!");
-        //}
-        #endregion
-
         #region Эксперименты с потоками №2
         private const string data_dir = "data";
         private static char[] __Separators = { ' ', '.', ',', '!', ';', '-', '(', ')', '[', ']', '{', '}' };
@@ -119,6 +101,15 @@ namespace TestConcole
         }
         #endregion
 
+        #region Синхронизация
+        private static void ThreadMethod()
+        { 
+            Console.WriteLine("Вторичный поток стартовал!");
+
+            Console.WriteLine("Вторичный поток завершился!");
+        }
+        #endregion
+
         static void Main(string[] args)
         {
             #region Работа с ресурсами
@@ -136,97 +127,68 @@ namespace TestConcole
             //}
             #endregion
 
-            #region Эксперименты с потоками №1
-
-            //var clock_thread = new Thread(StartConsoleHeaderClock);
-            //clock_thread.IsBackground = true;
-            //clock_thread.Priority = ThreadPriority.AboveNormal;
-            //clock_thread.Name = "Поток часов";
-
-            //clock_thread.Start();
-            //Console.WriteLine($"Идентификатор потока часов: {clock_thread.ManagedThreadId}");
-
-            //Console.ReadLine();
-            //__CanClockWork = false;
-            //Синхронизация потоков
-            //if (!clock_thread.Join(105))
-            //    clock_thread.Interrupt();
-
-            ////StartConsoleHeaderClock();
-
-            //Console.WriteLine("Главный поток завершён!");
-            //Console.ReadLine();
-            //Console.WriteLine("Главный поток выгружен");
-            #endregion
-
             #region Эксперименты с потоками №2
-            //var data_direstory = new DirectoryInfo(data_dir);
+            var data_direstory = new DirectoryInfo(data_dir);
 
-            //var words_count = 0;
-            //foreach (var file in data_direstory.GetFiles())
+            var words_count = 0;
+
+            var timer = new Stopwatch();
+
+            var files = data_direstory.GetFiles();
+
+            var manual_reset_event = new ManualResetEvent(false);
+            var auto_reset_event = new AutoResetEvent(false);
+
+            //var thread = new Thread(() => 
             //{
-            //    Console.WriteLine($"{file.Name} - {file.Length / 1024.0}");
-            //    words_count += GetWordsCount(file);
-            //}
+            //    Console.WriteLine("Вторичный поток стартовал!");
+            //    Thread.Sleep(500);
 
-            //Console.WriteLine($"Общее количество слов в романе \"Война и мир\" составляет {words_count} последовательно.");
+            //    Console.WriteLine("Вторичный поток ждёт разрешения на дальнейшую работу...");
+            //    manual_reset_event.WaitOne();
+            //    Console.WriteLine("Разрешение получено.");
 
-            //var files = data_direstory.GetFiles();
-
-            //var threads = new Thread[files.Length];
-            //words_count = 0;
-            //for (int i = 0; i < threads.Length; i++)
-            //{
-            //    var files_to_process = files[i];
-            //    threads[i] = new Thread(
-            //        () =>
-            //        {
-            //            var count = GetWordsCount(files_to_process);
-            //            lock (threads)
-            //                words_count += count;
-            //        });
-            //    threads[i].Start();
-            //}
-
-            //for (int i = 0; i < threads.Length; i++)
-            //    threads[i].Join();
-
-            //Console.WriteLine($"Число слов {words_count} параллельно.");
-
-            //var print_threads = new Thread[10];
-            //for (int i = 0; i < print_threads.Length; i++)
-            //{
-            //    var message = $"Thread {i + 1} message";
-            //    var thread = new Thread(() => PrintConsoleData(message, 10, 0));
-            //    thread.Start();
-            //    print_threads[i] = thread;
-            //}
-
-            //var words_count = data_direstory.GetFiles("*.txt").Sum(GetWordsCount);
-            //Console.WriteLine($"Число слов {words_count} linq.");
-
-            //var words_count = data_direstory.GetFiles("*.txt")
-            //    .AsParallel()
-            //    .Sum(GetWordsCount);
-            //Console.WriteLine($"Число слов {words_count} linq parallel.");
-
-            //Parallel.ForEach(files, file =>
-            //{
-            //    var count = GetWordsCount(file);
-            //    lock (files)
-            //        words_count += count;
+            //    Console.WriteLine("Вторичный поток завершился!");
             //});
-            //Console.WriteLine($"Число слов {words_count} параллельно.");
+            //thread.Start();
 
-            //Parallel.For(0, files.Length, i =>
-            //{
-            //    var file = files[i];
-            //    var count = GetWordsCount(file);
-            //    lock (files)
-            //        words_count += count;
-            //});
-            //Console.WriteLine($"Число слов {words_count} параллельно.");
+            //Console.WriteLine("Главный поток готов возобновить работу вторичного...");
+            //Console.ReadLine();
+            //manual_reset_event.Set();
 
+            var threads = new Thread[10];
+
+            for (var i = 0; i < threads.Length; i++)
+            {
+                var index = i;
+                var thread = new Thread(() => 
+                {
+                    Console.WriteLine($"Поток {index} запущен и ждёт разрешения.");
+                    Thread.Sleep(500);
+
+                    manual_reset_event.WaitOne();
+
+                    Thread.Sleep(500);
+                    Console.WriteLine($"Поток {index} выполнил свою работу.");
+                });
+                thread.Start();
+                threads[i] = thread;
+            }
+
+            Console.WriteLine("Главный поток готов разрешить работу...");
+            Console.ReadLine();
+
+            manual_reset_event.Set();
+
+            //var mutex = new Mutex(true, "Mutex name");
+            //mutex.WaitOne();
+            //mutex.ReleaseMutex();
+
+            //var semaphor = new Semaphore(0, 10); //Семафор на 10 входов
+            //semaphor.WaitOne();
+            //semaphor.Release();
+            
+            Console.WriteLine("Главный поток завершился!");
             Console.ReadKey();
             #endregion
         }
--- TestWPFWithoutMD/MainWindow.xaml.cs
+++ TestWPFWithoutMD/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace TestWPFWithoutMD
 {
@@ -7,6 +12,103 @@ namespace TestWPFWithoutMD
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private CancellationTokenSource _OperationCancellation;
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            button.IsEnabled = false;
+
+            _OperationCancellation?.Cancel();
+            var cancellation = new CancellationTokenSource();
+            _OperationCancellation = cancellation;
+
+            //DoProcess();
+            //DoProcessInThread();
+            IProgress<double> progress = new Progress<double>(p => ProgressB.Value = p);
+
+            var cancel = cancellation.Token;
+
+            try
+            {
+                var result = await Task.Run(() => DoWorkAsync(100, 100, progress, cancel), cancel);
+                ResultViewer.Text = result;
+            }
+            catch (TaskCanceledException)
+            {
+                Debug.WriteLine("Операция вычисления отменена!");
+                progress.Report(0);
+            }
+            button.IsEnabled = true;
+        }
+
+        private void DoProcessInThread()
+        {
+            var thread = new Thread(DoProcess)
+            {
+                IsBackground = true
+            };
+
+            thread.Start();
+        }
+
+        private void ProgressInformator(double Progress)
+        {
+            //ProgressB.Value = Progress;
+            if (ProgressB.Dispatcher.CheckAccess())
+                ProgressB.Value = Progress;
+            else
+                Dispatcher.Invoke(() => { ProgressB.Value = Progress; });
+        }
+
+        private void DoProcess()
+        {
+            var result = DoWork(100, 100, ProgressInformator);
+            //ResultViewer.Text = result;
+            if (ResultViewer.Dispatcher.CheckAccess())
+            {
+                ResultViewer.Text = result;
+            }
+            else
+                ResultViewer.Dispatcher.Invoke(() =>
+                {
+                    ResultViewer.Text = result;
+                });
+        }
+
+        private static string DoWork(int IterationCount, int Timeout, Action<double> ProgressInfo = null)
+        {
+            var thread_id = Thread.CurrentThread.ManagedThreadId;
+            for (int i = 0; i < IterationCount; i++)
+            {
+                Debug.WriteLine($"Итерации {i} - поток {thread_id}");
+                ProgressInfo?.Invoke((double)i / IterationCount);
+                Thread.Sleep(Timeout);
+            }
+
+            ProgressInfo?.Invoke(1);
+            return "Result" + DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private static async Task<string> DoWorkAsync(int IterationCount, int Timeout, IProgress<double> Progress, CancellationToken Cancel = default, Action<double> ProgressInfo = null)
+        {
+            var thread_id = Thread.CurrentThread.ManagedThreadId;
+            for (int i = 0; i < IterationCount; i++)
+            {
+                Cancel.ThrowIfCancellationRequested();
+                Debug.WriteLine($"Итерации {i} - поток {thread_id}");
+                Progress?.Report((double)i / IterationCount);
+                await Task.Delay(Timeout, Cancel);
+            }
+
+            return "Result" + DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _OperationCancellation?.Cancel();
         }
     }
 }
