a0c3e380062b2c4ab5a5eeb6295c04eeb4828f82
Геннадий Тертычный <gonzyck@gmail.com>
2021-01-12 15:36:41 +0300

    Поэксперементировал с сериализацией/десериализацией XML, переименованием, удалением, копированием файлов.

--- TestConcole/Program.cs
+++ TestConcole/Program.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
+using Spire.Doc;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,8 +10,11 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Resources;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 using TestConcole.Properties;
 
 namespace TestConcole
@@ -31,7 +36,7 @@ namespace TestConcole
                 StringEscapeHandling = StringEscapeHandling.EscapeNonAscii,
                 DefaultValueHandling = DefaultValueHandling.Ignore,
                 NullValueHandling = NullValueHandling.Ignore,
-                Formatting = Formatting.Indented
+                Formatting = Newtonsoft.Json.Formatting.Indented
                 //TypeNameHandling = TypeNameHandling.Auto,
                 //ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
                 //Formatting = Formatting.Indented,
@@ -103,10 +108,112 @@ namespace TestConcole
 
         #region Синхронизация
         private static void ThreadMethod()
-        { 
+        {
             Console.WriteLine("Вторичный поток стартовал!");
 
             Console.WriteLine("Вторичный поток завершился!");
+        }
+        #endregion
+
+        #region Работа с xml
+
+        private static void ChangeTheExstension()
+        {
+            var files = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory))
+                .Where(x => x.EndsWith(".xml"))
+                .ToArray();
+
+            foreach (var file in files)
+            {
+                //FileSystem.RenameFile(file, string.Format($"{Path.GetFileNameWithoutExtension(file)}.xls"));
+                //Console.WriteLine();
+                File.Move(file, string.Format($"{file.Remove(file.IndexOf(".xml"))}.xls"), true);
+            }
+        }
+
+        private static void GetFileNameFromDesktop()
+        {
+            Document mydoc = new Document();
+
+            XmlSerializer cnt = new XmlSerializer(typeof(string));
+            XmlWriterSettings xmlWriterSettings = new XmlWriterSettings()
+            {
+                Encoding = Encoding.Default,
+                NamespaceHandling = NamespaceHandling.OmitDuplicates,
+                WriteEndDocumentOnClose = true,
+                CheckCharacters = true,
+                Indent = true,
+                NewLineOnAttributes = true,
+                CloseOutput = true,
+                ConformanceLevel = ConformanceLevel.Document,
+            };
+
+            var XMLPropInfo = xmlWriterSettings.GetType().GetProperty("OutputMethod");
+            XMLPropInfo.SetValue(xmlWriterSettings, XmlOutputMethod.Xml, null);
+
+            string srRead;
+            string txt;
+
+            using (var sr = new FileStream(txt = "123.txt", FileMode.OpenOrCreate))
+            {
+                string text;
+
+                byte[] input;
+
+                text = new string(Enumerable
+                    .Range(1, 5)
+                    .Select(x => (char)x)
+                    .ToArray())
+                    .Aggregate(new StringBuilder("\r\n", txt.Length * 2 + 1),
+                        (sb, c) =>
+                            sb.Append($"{c}\r\n"),
+                        sb =>
+                            sb.ToString());
+
+                input = Encoding.Default.GetBytes(text);
+
+                sr.Write(input, 0, input.Length);
+
+                sr.Read(input, 0, input.Length);
+                srRead = Encoding.Default.GetString(input);
+                Console.WriteLine(srRead);
+            };
+
+            File.Delete(txt);
+
+            using (var sww = new StreamWriter(txt, true, Encoding.Default))
+            {
+                //var writer = new XmlTextWriter(sww)
+                //{
+                //    Formatting = System.Xml.Formatting.Indented,
+                //};
+
+                var writer = XmlWriter.Create(sww, xmlWriterSettings);
+
+                cnt.Serialize(writer, srRead);
+            }
+
+            //var FilesFromDesktop = Directory.GetFiles(PathDesktopDirectory, new string(new char[] { '.', 'x', 'm', 'l' }));
+            //var FilesFromDesktop = Directory.GetFiles(PathDesktopDirectory).Where(x => x.Contains(".xml"));
+
+            var FilesFromDesktop = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), new string(new char[] { '*', '.', 'x', 'm', 'l' }), System.IO.SearchOption.AllDirectories);
+
+            foreach (var item in FilesFromDesktop.Select(x => Path.GetFileName(x)))
+            {
+                if (!File.Exists(item))
+                    File.Copy(FilesFromDesktop.First(x => x.Contains(item)), item, true);
+
+                if (!item.Contains('('))
+                {
+                    mydoc.LoadFromFile(item, FileFormat.Xml);
+                    mydoc.SaveToFile("Test.doc", FileFormat.Doc);
+                    mydoc.SaveToFile("Test.pdf", FileFormat.PDF);
+                }
+
+                Console.WriteLine(item + " прочитан");
+                File.Delete(item);
+            }
+            Console.ReadKey();
         }
         #endregion
 
@@ -128,16 +235,16 @@ namespace TestConcole
             #endregion
 
             #region Эксперименты с потоками №2
-            var data_direstory = new DirectoryInfo(data_dir);
+            //var data_direstory = new DirectoryInfo(data_dir);
 
-            var words_count = 0;
+            //var words_count = 0;
 
-            var timer = new Stopwatch();
+            //var timer = new Stopwatch();
 
-            var files = data_direstory.GetFiles();
+            //var files = data_direstory.GetFiles();
 
-            var manual_reset_event = new ManualResetEvent(false);
-            var auto_reset_event = new AutoResetEvent(false);
+            //var manual_reset_event = new ManualResetEvent(false);
+            //var auto_reset_event = new AutoResetEvent(false);
 
             //var thread = new Thread(() => 
             //{
@@ -156,29 +263,29 @@ namespace TestConcole
             //Console.ReadLine();
             //manual_reset_event.Set();
 
-            var threads = new Thread[10];
+            //var threads = new Thread[10];
 
-            for (var i = 0; i < threads.Length; i++)
-            {
-                var index = i;
-                var thread = new Thread(() => 
-                {
-                    Console.WriteLine($"Поток {index} запущен и ждёт разрешения.");
-                    Thread.Sleep(500);
+            //for (var i = 0; i < threads.Length; i++)
+            //{
+            //    var index = i;
+            //    var thread = new Thread(() => 
+            //    {
+            //        Console.WriteLine($"Поток {index} запущен и ждёт разрешения.");
+            //        Thread.Sleep(500);
 
-                    manual_reset_event.WaitOne();
+            //        manual_reset_event.WaitOne();
 
-                    Thread.Sleep(500);
-                    Console.WriteLine($"Поток {index} выполнил свою работу.");
-                });
-                thread.Start();
-                threads[i] = thread;
-            }
+            //        Thread.Sleep(500);
+            //        Console.WriteLine($"Поток {index} выполнил свою работу.");
+            //    });
+            //    thread.Start();
+            //    threads[i] = thread;
+            //}
 
-            Console.WriteLine("Главный поток готов разрешить работу...");
-            Console.ReadLine();
+            //Console.WriteLine("Главный поток готов разрешить работу...");
+            //Console.ReadLine();
 
-            manual_reset_event.Set();
+            //manual_reset_event.Set();
 
             //var mutex = new Mutex(true, "Mutex name");
             //mutex.WaitOne();
@@ -187,10 +294,16 @@ namespace TestConcole
             //var semaphor = new Semaphore(0, 10); //Семафор на 10 входов
             //semaphor.WaitOne();
             //semaphor.Release();
-            
-            Console.WriteLine("Главный поток завершился!");
-            Console.ReadKey();
+
+            //Console.WriteLine("Главный поток завершился!");
+            //Console.ReadKey();
             #endregion
+
+            #region Работа с xml
+            //ChangeTheExstension();
+            //GetFileNameFromDesktop();
+            #endregion
+
         }
     }
 }
